7a853ab63a890b5f3f1ffcce71ec39eaa7f687b6
sezin inyayla <95285144+sezins@users.noreply.github.com>
2022-04-23 22:22:45 +0300

    ++

---
+++ DataAccessLayer/Repository/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repository
+{
+    
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        public void Delete(T t)
+        {
+            using var c = new Context();
+            c.Remove(t);
+            c.SaveChanges();
+        }
+
+        public T GetByID(int ID)
+        {
+            using var c = new Context();
+            return c.Set<T>().Find(ID);
+        }
+
+        public List<T> GetList()
+        {
+            using var c = new Context();
+            return c.Set<T>().ToList();
+        }
+
+        public void Insert(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
